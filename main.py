0eaa52ed679f369c338987a93294896456772068
SCurry <61257658+Born-A-Bot@users.noreply.github.com>
2025-08-26 15:52:11 -0400

    Add files via upload
    no_redirect

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-#import library for qrcode
+#import library 
 import qrcode
+import requests
 
 #create variable and source - url
-img = qrcode.make('https://github.com/Born-A-Bot/My-PyPort-QRCode')
+
+url = 'https://github.com/Born-A-Bot/My-PyPort-QRCode' 
+
+img = qrcode.make(url)
 
 #save the image and assign a name to the file
-img.save("git_py.png")
+img.save("C://Users/seren/Documents/scrooples/pyport/qr_code/git_py.png")
 
 #generate message when task complete, qrcode generated and saved
 print('image saved')
+
+response = requests.get(url, allow_redirects=False)
+
+print(f"Status Code: {response.status_code}")
+
+if "Location" in response.headers:
+    print(f"Redirect Location: {response.headers['Location']}")
+else:
+    print("No redirect decteced.")
+
+
+
